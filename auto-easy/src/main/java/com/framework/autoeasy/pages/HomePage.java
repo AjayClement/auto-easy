b7eb8482f49ac912b1cd4115ab5aeeb11ea84c09
Ajay Clement <clementajay@gmail.com>
2019-06-24 01:20:47 +0530

    updates

--- auto-easy/src/main/java/com/framework/autoeasy/pages/HomePage.java
+++ auto-easy/src/main/java/com/framework/autoeasy/pages/HomePage.java
@@ -1,31 +1,28 @@
 package com.framework.autoeasy.pages;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
 import com.framework.autoeasy.reusablelibrary.ReusableLibrary;
 
-public class HomePage{
+public class HomePage {
 
-	private WebDriver driver; 
-	private static By homePageLogo = By.xpath("//a[@title='My Store']");
+	private WebDriver driver;
 	private ReusableLibrary objcommon = new ReusableLibrary();
+
+	private final By HOME_PAGE = By.xpath("//a[@title='My Store']");
+
 	public HomePage(WebDriver driver) {
-		
-		 this.driver = driver;
-		
+
+		this.driver = driver;
+
 	}
-	
-	
+
 	public boolean homePageLogo() {
-		
-		WebElement logo = driver.findElement(homePageLogo);
-		return ReusableLibrary.isElementDisplayed(logo);
+
+		WebElement logo = driver.findElement(HOME_PAGE);
+		return objcommon.isElementDisplayed(logo);
 	}
-	
-	
+
 }
